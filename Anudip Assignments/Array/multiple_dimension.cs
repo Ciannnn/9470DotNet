582466d3d184675edbb6c98bf0c4619564428abd
Ciannnn <94827579+Ciannnn@users.noreply.github.com>
2021-12-27 16:51:23 +0530

    Add files via upload

---
+++ Anudip Assignments/Array/multiple_dimension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Studytonight
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = new int[] { 10, 11, 100, 101, 110 };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            /*foreach(int a in arr)
+            {
+            Console.WriteLine(a);
+            }*/
+            Console.ReadLine();
+        }
+    }
