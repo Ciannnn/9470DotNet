ce5f249ec547e4f023ba2e3a82ed42fcb27d417d
Ciannnn <94827579+Ciannnn@users.noreply.github.com>
2022-01-11 18:00:24 +0530

    Add files via upload

---
+++ Anudip Practicals/Exercise/display_rec.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class display_rec
+{
+    public static void Main()
+    {
+        int x;
+
+        Console.Write("Enter a number: ");
+        x = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("{0}{0}{0}", x);
+        Console.WriteLine("{0} {0}", x);
+        Console.WriteLine("{0} {0}", x);
+        Console.WriteLine("{0} {0}", x);
+        Console.WriteLine("{0}{0}{0}", x);
+    }
+}
