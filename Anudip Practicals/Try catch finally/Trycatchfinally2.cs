e7e756a4b11b32a394014355054573a778326d64
Ciannnn <94827579+Ciannnn@users.noreply.github.com>
2022-01-13 20:58:45 +0530

    Add files via upload

---
+++ Anudip Practicals/Try catch finally/Trycatchfinally2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Anudip_Exercise
+{
+    class Trycatchfinally2
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] myNumbers = { 1, 2, 3 };
+                Console.WriteLine(myNumbers[10]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something went wrong.");
+            }
+        }
+
+    }
+}
---
+++ Anudip Practicals/Try catch finally/Trycatchfinally3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Anudip_Exercise
+{
+    class Trycatchfinally3
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] myNumbers = { 1, 2, 3 };
+                Console.WriteLine(myNumbers[10]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something went wrong.");
+            }
+            finally
+            {
+                Console.WriteLine("The 'try catch' is finished.");
+            }
+        }
+    }
+}
