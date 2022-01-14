c346d9ad28daa3fc098687ad90b16269da126a36
Ciannnn <94827579+Ciannnn@users.noreply.github.com>
2022-01-14 13:04:13 +0530

    Create Singlecast_DelEvents.cs

---
+++ Anudip Practicals/Delegates/Singlecast_DelEvents.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace Anudip_Exercise
+{
+    class Multicast
+    {
+        public delegate void MyDelegate(int a);
+         public class XX
+         {
+        public event MyDelegate MyEvent;
+
+        public void RaiseEvent()
+         {
+         MyEvent(20);
+        Console.WriteLine("Event Raised");
+         }
+
+        public void Display(int x)
+         {
+        Console.WriteLine("Display Method {0}", x);
+         }
+         }
+
+         static void Main(string[] args)
+         { 
+          XX obj = new XX();
+         obj.MyEvent += new MyDelegate(obj.Display);
+
+         obj.RaiseEvent();
+        Console.ReadLine();
+         }
+    }
+}
