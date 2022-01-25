60ce9987c72678c15acdd233a47e1a9c8bcf1d6c
Ciannnn <94827579+Ciannnn@users.noreply.github.com>
2022-01-25 16:08:36 +0530

    Create UpdateView.sql

---
+++ Anudip Practicals/SQL/UpdateView.sql
@@ -0,0 +1,36 @@
+create table emp(
+empid int primary key not null,
+empname varchar(25) not null,
+empcity varchar(25) not null,
+empsal varchar(25) not null,
+empphno varchar(10));
+
+insert into emp values(1,'cianna','uae',50000,9765824175);
+insert into emp values(2,'abc','usa',30000,9158324175);
+insert into emp values(3,'xyz','abu',40000,9658114175);
+insert into emp values(4,'qrs','fra',20000,9847324175);
+insert into emp values(5,'uvw','dub',10000,9768376175);
+
+select * from emp;
+
+--view 
+
+create view emp_view as select empname,empcity from emp;
+
+select * from emp_view;
+
+--update view
+update emp_view set empname='abc' where empcity='asd'; 
+
+--delete view
+delete from emp_view where empname='ap';
+
+
+
+
+
+
+
+
+
+
